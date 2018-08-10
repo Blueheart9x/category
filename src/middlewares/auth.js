a63f4c70e4100a7f6a090f674f1da1054d6e3721
Duong Ngoc Dien <duongngocdien@gmail.com>
2018-08-10 22:12:49 +0700

    Refactor source code #2

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,4 @@
 import RequestUtil from '../utils/request'
-import ErrorResponseBody from '../common/models/response/error_response_body'
 import { ResponseCode } from '../common/constants/response'
 import CommonError from '../common/errors/common_error'
 import jwt from 'jsonwebtoken'
@@ -8,12 +7,11 @@ export const verifyAuth = (req, res, next) => {
     const token = RequestUtil.getTokenFromRequest(req)
 
     if (!token) {
-        const responseBody = new ErrorResponseBody('No token provided')
-        return res.status(ResponseCode.UNAUTHORIZED).json(responseBody)
+        throw new CommonError(ResponseCode.UNAUTHORIZED, 'No token provided')
     }
 
     try {
-        const decodedToken = getDecodedToken(req, token)
+        const decodedToken = decodeToken(req, token)
         req.decoded = decodedToken
         next()
     } catch (error) {
@@ -21,7 +19,7 @@ export const verifyAuth = (req, res, next) => {
     }
 }
 
-const getDecodedToken = (req, token) => {
+const decodeToken = (req, token) => {
     const secret = req.app.get("secret")
     return jwt.verify(token, secret)
 }
