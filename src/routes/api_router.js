270637c178fda7de6a04859741618417dd46a993
Duong Ngoc Dien <duongngocdien@ato.vn>
2019-02-14 09:51:02 +0700

    Refactor source code #n

--- src/routes/api_router.js
+++ src/routes/api_router.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import * as authController from "../controllers/auth_controller.js"
+import * as authController from "../controllers/auth_controller"
 import * as authMiddleware from "../middlewares/auth_middleware"
 const router = Router()
 
